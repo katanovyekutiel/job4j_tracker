08f1f8d339b35ccdfde99b6561fab21376540d86
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-29 18:02:51 +0300

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/tracker/oop/BallStory.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+        ball.outwit(hare, wolf, fox);
+        hare.eat(ball);
+        wolf.eat(ball);
+        fox.eat(ball);
+    }
+}
