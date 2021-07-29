b5f015388eb08a4ef6c028e286d072d28b1ac1bc
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-29 12:28:04 +0300

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.oop;
+
+public class Cat {
+
+    private String name;
+
+    private String food;
+
+    public void show() {
+        System.out.println(this.food);
+        System.out.println(this.name);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Name is Gav");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Name is Black");
+        black.show();
+    }
+}
