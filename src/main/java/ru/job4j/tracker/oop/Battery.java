db795c83911951806c2e1e995b26225ca46bd45b
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-29 22:59:52 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/tracker/oop/Battery.java
@@ -0,0 +1,24 @@
+package ru.job4j.tracker.oop;
+
+public class Battery {
+    private int value;
+
+    public Battery(int size) {
+        this.value = size;
+    }
+
+    public void exchange(Battery another) {
+        this.value = this.value + another.value;
+        another.value = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery one = new Battery(10);
+        Battery two = new Battery(5);
+        System.out.println("battery one : " + one.value
+                + "    battery two : " + two.value);
+        two.exchange(one);
+        System.out.println("battery one : " + one.value
+                + "    battery two : " + two.value);
+    }
+}
