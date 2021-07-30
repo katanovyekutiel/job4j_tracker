f7a41520c6134970cca462683d81f0204c18b36c
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-30 15:26:26 +0300

    3. Перегрузить метод max для трех чисел [#122644 #183486]

--- src/main/java/ru/job4j/tracker/oop/Max.java
+++ src/main/java/ru/job4j/tracker/oop/Max.java
@@ -1,12 +1,17 @@
 package ru.job4j.tracker.oop;
 
 public class Max {
-    public static int max(int first, int second, int third, int forth) {
-        return max(first, max(second, max(third, forth)));
+    public static int max(int left, int right) {
+        boolean condition = left > right;
+        int rsl = condition ? left : right;
+        return rsl;
+    }
+
+    public static int maxFour(int first, int second, int third, int fourth) {
+        return max(first, max(second, max(third, fourth)));
     }
 
     public static void main(String[] args) {
-        int msg = Max.max(1, 2, 3, 4);
-        System.out.println(msg);
+        System.out.println(Max.maxFour(1, 2, 3, 4));
     }
 }
