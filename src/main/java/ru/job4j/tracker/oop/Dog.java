f238aa54e02f475d2a08ebf79ae9cd2b3672ba1e
katanovyekutiel <katanovyekutiel@gmail.com>
2021-07-30 07:27:32 +0300

    jjjjjj

---
+++ src/main/java/ru/job4j/tracker/oop/Dog.java
@@ -0,0 +1,17 @@
+package ru.job4j.tracker.oop;
+
+public class Dog {
+
+    public String sound() {
+        String voice = "gav-gav";
+        return voice;
+    }
+
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+        String say = polkan.sound();
+        System.out.println("Polkan say : " + say);
+    }
+}
--- src/main/java/ru/job4j/tracker/oop/Student.java
+++ src/main/java/ru/job4j/tracker/oop/Student.java
@@ -2,21 +2,17 @@ package ru.job4j.tracker.oop;
 
 public class Student {
 
-    public void song(){
-        System.out.println("I believe I can fly");
+    public void song(String song) {
+        System.out.println("I believe, I can fly");
     }
 
-    public void music() {
-        System.out.println("Tra tra tra");
+    public void music(String lyrics) {
+        System.out.println("I can sing a song : " + lyrics);
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.song();
-        petya.song();
-        petya.song();
+        String song = "I believe, I can fly";
+        petya.music(song);
     }
 }
