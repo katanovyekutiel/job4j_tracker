1233d976c9e251979b7404e65dda40eb6a015c8f
katanovyekutiel <katanovyekutiel@gmail.com>
2021-08-02 01:32:18 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,24 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public void printInfo() {
+        System.out.println("Id is : " + id);
+        System.out.println("Name is : " + name);
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
     public int getId() {
         return id;
     }
@@ -18,5 +36,14 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item one = new Item();
+        one.printInfo();
+        Item two = new Item("Yekutiel");
+        two.printInfo();
+        Item three = new Item(3, "Yekutiel");
+        three.printInfo();
     }
 }
