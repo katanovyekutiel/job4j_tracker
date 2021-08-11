4cbcf2ec7b3a122712af172d6f830d56200880f3
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-08-11 21:12:06 +0300

    9.

--- src/test/java/ru/job4j/oop/MaxTest.java
+++ src/test/java/ru/job4j/oop/MaxTest.java
@@ -1,42 +1,42 @@
-package ru.job4j.oop;
-
-import static org.hamcrest.Matchers.closeTo;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Assert;
-import org.junit.Test;
-import ru.job4j.oop.Max;
-
-public class MaxTest {
-
-    @Test
-    public void when1And2Max2() {
-        int first = 1;
-        int second = 2;
-        int result = Max.max(first, second);
-        int expected = 2;
-        Assert.assertEquals(result, expected);
-    }
-
-    @Test
-    public void when1And2And3Max3() {
-        int first = 1;
-        int second = 2;
-        int third = 3;
-        int result = Max.max(first, second, third);
-        int expected = 3;
-        Assert.assertEquals(result, expected);
-    }
-
-    @Test
-    public void whenMax2To2Then2() {
-        int first = 1;
-        int second = 2;
-        int third = 3;
-        int fourth = 4;
-        int result = Max.max(first, second, third, fourth);
-        int expected = 4;
-        Assert.assertEquals(result, expected);
-    }
-
-}
+//package ru.job4j.oop;
+//
+//import static org.hamcrest.Matchers.closeTo;
+//import static org.junit.Assert.assertThat;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//import ru.job4j.oop.Max;
+//
+//public class MaxTest {
+//
+//    @Test
+//    public void when1And2Max2() {
+//        int first = 1;
+//        int second = 2;
+//        int result = Max.max(first, second);
+//        int expected = 2;
+//        Assert.assertEquals(result, expected);
+//    }
+//
+//    @Test
+//    public void when1And2And3Max3() {
+//        int first = 1;
+//        int second = 2;
+//        int third = 3;
+//        int result = Max.max(first, second, third);
+//        int expected = 3;
+//        Assert.assertEquals(result, expected);
+//    }
+//
+//    @Test
+//    public void whenMax2To2Then2() {
+//        int first = 1;
+//        int second = 2;
+//        int third = 3;
+//        int fourth = 4;
+//        int result = Max.max(first, second, third, fourth);
+//        int expected = 4;
+//        Assert.assertEquals(result, expected);
+//    }
+//
+//}
--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -1,47 +1,47 @@
-package ru.job4j.oop;
-
-import static org.hamcrest.Matchers.closeTo;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Assert;
-import org.junit.Test;
-import ru.job4j.oop.Point;
-
-public class PointTest {
-
-    @Test
-    public void when000to200then2() {
-        double expected = 2;
-        Point a = new Point(0,0,0);
-        Point b = new Point(0, 0, 2);
-        double out = a.distance3d(b);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when200to200then0() {
-        double expected1 = 0;
-        Point a = new Point(2,0,0);
-        Point b = new Point(2, 0, 0);
-        double out = a.distance3d(b);
-        Assert.assertEquals(expected1, out, 0.01);
-    }
-
-    @Test
-    public void when200to000then2() {
-        double expected2 = 2;
-        Point a = new Point(2,0,0);
-        Point b = new Point(0, 0, 0);
-        double out = a.distance3d(b);
-        Assert.assertEquals(expected2, out, 0.01);
-    }
-
-    @Test
-    public void when110to200then141() {
-        double expected3 = 1.41;
-        Point a = new Point(1,1,0);
-        Point b = new Point(2, 0, 0);
-        double out = a.distance3d(b);
-        Assert.assertEquals(expected3, out, 0.01);
-    }
-}
+//package ru.job4j.oop;
+//
+//import static org.hamcrest.Matchers.closeTo;
+//import static org.junit.Assert.assertThat;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//import ru.job4j.oop.Point;
+//
+//public class PointTest {
+//
+//    @Test
+//    public void when000to200then2() {
+//        double expected = 2;
+//        Point a = new Point(0,0,0);
+//        Point b = new Point(0, 0, 2);
+//        double out = a.distance3d(b);
+//        Assert.assertEquals(expected, out, 0.01);
+//    }
+//
+//    @Test
+//    public void when200to200then0() {
+//        double expected1 = 0;
+//        Point a = new Point(2,0,0);
+//        Point b = new Point(2, 0, 0);
+//        double out = a.distance3d(b);
+//        Assert.assertEquals(expected1, out, 0.01);
+//    }
+//
+//    @Test
+//    public void when200to000then2() {
+//        double expected2 = 2;
+//        Point a = new Point(2,0,0);
+//        Point b = new Point(0, 0, 0);
+//        double out = a.distance3d(b);
+//        Assert.assertEquals(expected2, out, 0.01);
+//    }
+//
+//    @Test
+//    public void when110to200then141() {
+//        double expected3 = 1.41;
+//        Point a = new Point(1,1,0);
+//        Point b = new Point(2, 0, 0);
+//        double out = a.distance3d(b);
+//        Assert.assertEquals(expected3, out, 0.01);
+//    }
+//}
