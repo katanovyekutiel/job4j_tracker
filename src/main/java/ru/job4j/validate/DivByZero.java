0a7c770071a1ddc29419ae3bb720274a20a50cb4
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-08-11 17:47:56 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/validate/DivByZero.java
+++ src/main/java/ru/job4j/validate/DivByZero.java
@@ -2,11 +2,16 @@ package ru.job4j.validate;
 
 public class DivByZero {
     public static int div(int first, int second) {
-        return  first / second;
+        int rsl = -1;
+        if (second == 0) {
+            System.out.println("Div by 0, return def value -1");
+        } else {
+            rsl = first / second;
+        }
+        return rsl;
     }
-
     public static void main(String[] args) {
-        int rsl = div(10, 5);
+        int rsl = div(10, 0);
         System.out.println(rsl);
     }
 }
