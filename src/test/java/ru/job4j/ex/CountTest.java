1b9f4a01a8072ebfbb32aeeffd88f181bf3c982f
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-09-14 19:37:58 +0300

    0.3. Тестирование исключений с junit [#219366]

---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class CountTest {
+
+    @Test
+    public void when0to2then3() {
+        int rsl = Count.add(0, 3);
+        assertThat(rsl, is(3));
+    }
+}
