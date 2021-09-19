c05d95fa7e072e9ab18d6eb1046a3fc4be86a4ec
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-09-19 20:19:14 +0300

    0.6.

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,10 +1,10 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (int i = 0; i < value.length; i++) {
-            if (value[i].equals(key)) {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].equals(key)) {
                 rsl = i;
                 break;
             } else if (rsl == -1) {
@@ -13,6 +13,28 @@ public class FindEl {
         }
         return rsl;
     }
+
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        /* if contains throw ElementAbuseException */
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         try {
             indexOf(new String[]{"hello", "hi", "shalom"}, "helo");
--- src/test/java/ru/job4j/ex/CountTest.java
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -1,8 +1,15 @@
 package ru.job4j.ex;
 
 import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class CountTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenStartGreaterThenFinish() {
+        Count.add(10, 2);
+    }
 
     @Test
     public void when0to2then3() {
