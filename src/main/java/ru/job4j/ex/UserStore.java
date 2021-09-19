96c2153e818e97e0e27bd7128d703cd9e7a7607d
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-09-19 17:24:28 +0300

    0.4. Пользовательские исключения. [#219367 #204954]

---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,39 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (int i = 0; i < users.length; i++) {
+          if () {
+              throw new UserNotFoundException("User not found.");
+          }
+        }
+        return null;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user == false) {
+            throw new UserInvalidException("User not valid");
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        User user = null;
+        try {
+            user = findUser(users, "Petr Arsentev");
+        } catch (UserNotFoundException ea) {
+            ea.printStackTrace();
+        }
+        try {
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException e) {
+            e.printStackTrace();
+        }
+    }
+}
