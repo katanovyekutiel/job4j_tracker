ff6e5d99901cb815e70410d33b77615141a79c78
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-09-29 16:11:41 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #205361]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -1,40 +1,36 @@
-//package ru.job4j.ex;
-//
-//public class UserStore {
-//
-//    public static User findUser(User[] users, String login) throws UserNotFoundException {
-//        for (int i = 0; i < users.length; i++) {
-//          if () {
-//              throw new UserNotFoundException("User not found.");
-//          }
-//        }
-//        return null;
-//    }
-//
-//    public static boolean validate(User user) throws UserInvalidException {
-//        if (user == false) {
-//            throw new UserInvalidException("User not valid");
-//        }
-//        return false;
-//    }
-//
-//    public static void main(String[] args) {
-//        User[] users = {
-//                new User("Petr Arsentev", true)
-//        };
-//        User user = null;
-//        try {
-//            user = findUser(users, "Petr Arsentev");
-//        } catch (UserNotFoundException ea) {
-//            ea.printStackTrace();
-//        }
-//        try {
-//            if (validate(user)) {
-//                System.out.println("This user has an access");
-//            }
-//        } catch (UserInvalidException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//}
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        for (User user: users) {
+          if (user.getUsername().equals(login)) {
+              return user;
+          }
+        }
+        throw new UserNotFoundException("Такого пользователя нет в списке");
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("Не подходящее имя пользователя");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access.");
+            }
+        } catch (UserInvalidException ui) {
+            System.out.println("Не подходящий пользователь");
+        } catch (UserNotFoundException ue) {
+            System.out.println("Имя пользователя не найдено");
+        }
+    }
+}
