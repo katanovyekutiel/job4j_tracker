7d291fb97da3f3baf13288cb0152d3a7f70863a3
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-10-03 00:02:05 +0300

    1. Singleton [#94606 #211498]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -3,7 +3,16 @@ package ru.job4j.tracker;
 public class SingleTracker {
     private Tracker tracker = new Tracker();
 
+    private SingleTracker() {
 
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
 
     public Item add(Item item) {
         return tracker.add(item);
