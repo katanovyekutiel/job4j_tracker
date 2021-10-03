7ea65d771e87f8c4e52f43ccce0f268cd362632e
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-10-03 16:26:02 +0300

    ю

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,40 +1,40 @@
-package ru.job4j.tracker;
-
-public class SingleTracker {
-    private Tracker tracker = new Tracker();
-
-    private SingleTracker() {
-
-    }
-
-    public static SingleTracker getInstance() {
-        if (instance == null) {
-            instance = new SingleTracker();
-        }
-        return instance;
-    }
-
-    public Item add(Item item) {
-        return tracker.add(item);
-    }
-
-    public Item findById(int id) {
-        return null;
-    }
-
-    public Item findByName(String name) {
-        return tracker.findByName(String name);
-    }
-
-    public Item findAll() {
-        return tracker.findAll();
-    }
-
-    public Item replace(int id, Item item) {
-        return tracker.replace(id, item);
-    }
-
-    public Item delete(int id) {
-        return tracker.delete(id);
-    }
-}
+//package ru.job4j.tracker;
+//
+//public class SingleTracker {
+//    private Tracker tracker = new Tracker();
+//
+//    private SingleTracker() {
+//
+//    }
+//
+//    public static SingleTracker getInstance() {
+//        if (instance == null) {
+//            instance = new SingleTracker();
+//        }
+//        return instance;
+//    }
+//
+//    public Item add(Item item) {
+//        return tracker.add(item);
+//    }
+//
+//    public Item findById(int id) {
+//        return null;
+//    }
+//
+//    public Item findByName(String name) {
+//        return tracker.findByName(name);
+//    }
+//
+//    public Item findAll() {
+//        return tracker.findAll();
+//    }
+//
+//    public Item replace(int id, Item item) {
+//        return tracker.replace(id, item);
+//    }
+//
+//    public Item delete(int id) {
+//        return tracker.delete(id);
+//    }
+//}
