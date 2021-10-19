11cd89a6c942ebc709aafe1c91c2b354f5737eb2
katanovyekutiel <87259693+katanovyekutiel@users.noreply.github.com>
2021-10-19 18:43:08 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String orig : origin) {
+            for (String tex : text) {
+                
+            }
+        }
+        return rsl;
+    }
+}
